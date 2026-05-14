89ae725847722871f99178b92646ed40d627a5fb
Kayakuu <hoang.nth17@gmail.com>
2026-05-14 10:01:44 +0700

    fix: remove duplicate axios/Echo/Pusher declarations in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -58,24 +58,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // import './echo';
 // import './echo-admin';
-    import axios from 'axios';
-    window.axios = axios;
-    import toastr from 'toastr'; // Ensure toastr is installed via npm
-    // Mặc định: cho Chatify
-    window.Pusher = require('pusher-js');
-
-    window.Echo = new Echo({
-        broadcaster: 'pusher',
-        key: import.meta.env.VITE_PUSHER_APP_KEY,
-        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-        forceTLS: true,
-    });
-
-    // Khác biệt: tạo 1 Echo riêng cho Notifications
-    window.NotifyEcho = new Echo({
-        broadcaster: 'pusher',
-        key: import.meta.env.VITE_PUSHER_NOTIFY_KEY,
-        cluster: import.meta.env.VITE_PUSHER_NOTIFY_CLUSTER,
-        forceTLS: true,
-    });
-    import './noti';
+import './noti';
